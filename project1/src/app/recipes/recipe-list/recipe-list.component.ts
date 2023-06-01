9c4dd52c5c79de9eae201c327e974e8389fa72f4
Igor Pires <igor.pires@levty.com>
2023-06-01 14:43:51 -0300

    til 52

--- project1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.sass']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [new Recipe("A Test Recipe","Nur einen Test","https://comidinhasdochef.com/wp-content/uploads/2016/06/Receita-de-Almoc%CC%A7o-em-12-minutos-00.png")];
 
+  constructor() {}
+
+  ngOnInit(){
+
+  }
 }
