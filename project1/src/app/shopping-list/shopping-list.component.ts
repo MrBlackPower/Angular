360ae04d4098755c92f682ea170d51a35a324f6e
Igor Pires <igor.pires@levty.com>
2023-12-04 19:57:37 -0300

    till 97

--- project1/src/app/shopping-list/shopping-list.component.ts
+++ project1/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.sass']
 })
 export class ShoppingListComponent {
+  ingredients : Ingredient[] = [
+    new Ingredient("Cheese",4),
+    new Ingredient("Tomatoes",4),
+    new Ingredient("Bacon",2)
+  ];
+  
+  constructor(){
 
+  }
+
+  ngOnInit(){
+
+  }
 }
