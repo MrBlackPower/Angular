bfccca83eb6f4f9bf08f457de1a81b35fbf13dfc
Igor Pires <igor.pires@levty.com>
2023-12-04 17:44:05 -0300

    Finish Ass 4

--- cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-game-control',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent {
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
 
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
+
+  onStartGame(){
+    this.interval = setInterval(()=>{
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    },1000);
+  }
+
+  onPauseGame(){
+    clearInterval(this.interval);
+  }
 }
